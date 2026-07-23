124ca1710139a6f6cd291921156d996e0f5405b9
devbylin <saulhstore@gmail.com>
2026-07-22 21:04:16 -0500

    dibujar cuadricula// 2 for//

--- serpiente.js
+++ serpiente.js
@@ -3,6 +3,8 @@
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
 
+    const TAMANIO_CELDA = 25;
+
 
     
 
@@ -19,6 +21,26 @@
 
     function dibujarTodo() {
       limpiarCanvas();
+      dibujarTablero();
+    }
+    function dibujarTablero(){
+      ctx.strokeStyle = "rgba(99, 103, 101, 0.93)";
+      ctx.lineWidth = 1;
+
+      for(let x = 0; x <= canvas.width; x += TAMANIO_CELDA){
+        ctx.beginPath();
+        ctx.moveTo(x , 1);
+        ctx.lineTo(x , canvas.height);
+        ctx.stroke();
+      }
+      for(let y = 0; y <= canvas.height; y += TAMANIO_CELDA){
+        ctx.beginPath();
+        ctx.moveTo(1 , y);
+        ctx.lineTo(canvas.width , y);
+        ctx.stroke();
+      }
+      
+
     }
 
 
